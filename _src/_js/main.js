6678940f6f45489f386ded4608faf430044d0816
Kenneth Wang <kennethwang14@gmail.com>
2015-10-29 23:06:01 -0700

    Home layout

--- _src/_js/main.js
+++ _src/_js/main.js
@@ -1,6 +1,6 @@
 $(function(){
       $(".introduction__typewriter").typed({
-        strings: ["First sentence.", "Second sentence."],
+        strings: ["I am a UI/UX designer and frontend developer living and working in Bellevue, Washington.", "Second sentence."],
         typeSpeed: 0
       });
  });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,2 @@
-$(function(){$(".introduction__typewriter").typed({strings:["First sentence.","Second sentence."],typeSpeed:0})});
+$(function(){$(".introduction__typewriter").typed({strings:["I am a UI/UX designer and frontend developer living and working in Bellevue, Washington.","Second sentence."],typeSpeed:0})});
 //# sourceMappingURL=main.js.map
