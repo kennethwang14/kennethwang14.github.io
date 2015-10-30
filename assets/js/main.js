ef513e64a741771e108adab534c398ad571102f3
Kenneth Wang <kennethwang14@gmail.com>
2015-10-30 03:06:38 -0700

    Home layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,2 @@
-$(function(){$(".introduction__typewriter").typed({strings:["$ I am a UI/UX designer and frontend developer living and working in Bellevue, Washington.","$ I have a passion for elegant design, beautiful interfaces, and using technology to make the world a better place."],typeSpeed:0})});
+$(function(){$(".introduction__typewriter").typed({strings:["> I am a UI/UX designer and frontend developer living and working in Bellevue, Washington.","> I have a passion for elegant design, beautiful interfaces, and using technology to make the world a better place."],typeSpeed:0})});
 //# sourceMappingURL=main.js.map
