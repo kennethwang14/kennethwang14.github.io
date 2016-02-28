c2ca61fc8c06dc1cc2537099cc0302b1b2b6f25d
Kenneth Wang <kennethwang14@gmail.com>
2016-02-27 21:27:48 -0800

    Navigation bar padding fix

--- _src/_js/main.js
+++ _src/_js/main.js
@@ -1,7 +1,7 @@
 $(function(){
       $(".introduction__typewriter").typed({
         strings: ["> ^1500 I am a UI/UX designer and frontend developer living and working in Bellevue, Washington.",
-        "$ ^500 I have a passion for elegant design, beautiful interfaces, and using technology to make the world a better place."],
+        "> ^500 I have a passion for elegant design, beautiful interfaces, and using technology to make the world a better place."],
         typeSpeed: 0,
         backDelay: 1250,
         contentType: 'text',
