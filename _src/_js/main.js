daeff6944ae2d8a48c9f88e1bb5d89e0e9d91a76
Kenneth Wang <kennethwang14@gmail.com>
2016-02-28 21:38:03 -0800

    Fixed URL linking

--- _src/_js/main.js
+++ _src/_js/main.js
@@ -15,3 +15,34 @@ $(function(){
       $(".homeAnimation5").delay(700).animate({ opacity: 1 }, 1200);​
       $(".typed-cursor").delay(700).css({ "display" : "inline" });​
  });
+
+// http://unheap.com
+
+$(function() {
+  $('.browsehappy').click(function() {
+    $(this).slideUp();
+  });
+});
+
+$(function() {
+ $('.magnific').magnificPopup({
+  type: 'image',
+  mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+  zoom: {
+    enabled: true, // By default it's false, so don't forget to enable it
+
+    duration: 300, // duration of the effect, in milliseconds
+    easing: 'ease-in-out', // CSS transition easing function
+
+    // The "opener" function should return the element from which popup will be zoomed in
+    // and to which popup will be scaled down
+    // By defailt it looks for an image tag:
+    opener: function(openerElement) {
+      // openerElement is the element on which popup was initialized, in this case its <a> tag
+      // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+      return openerElement.is('img') ? openerElement : openerElement.find('img');
+    }
+  }
+  });
+});
