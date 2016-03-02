0584304259cde75d5d21f38e1b3d7ec3e8517928
Kenneth Wang <kennethwang14@gmail.com>
2016-03-01 23:42:06 -0800

    Test contact

--- _src/_js/main.js
+++ _src/_js/main.js
@@ -34,54 +34,7 @@ $(function() {
   });
 });
 
-var btn = $('.contact');
-var background = $('.background');
-
-
-var width = 0;
-var height = 0;
-var r = 0;
-setSize();
-
-function setSize() {
-  width = $(window).width();
-	height = $(window).height();
-  r = Math.sqrt(width * width + height * height);
-}
-$(window).resize(setSize);
-
-btn.click(function(e) {
-  btn.removeClass('current');
-  $(this).addClass('current');
-  var circle = $("<div unselectable='on' id='circle'></div>");
-  background.append(circle);
-  circle.css({
-    position: 'absolute',
-    'background-color': 'rgb(33,150,243)',
-    width: 0,
-    height: 0,
-    "border-radius": "50%",
-    left: e.pageX,
-    top: e.pageY,
-    "z-index": "100",
-    'margin-left': 0,
-    'margin-top': 0,
-    'webkit-user-select': 'none',
-    '-moz-user-select': 'none',
-    '-ms-user-select': 'none'
-  });
-  circle.animate({
-    width: (r * 2),
-    height: (r * 2),
-   	'margin-left': -r,
-    'margin-top': -r
-  }, {
-    duration: 600,
-    queue: false,
-    complete: function() {
-     circle.parent().css('background-color',
-                    $(this).css('background-color'));
-     //circle.detach();
-    }
-  });
-});
+$(".contact").on('click', function(){
+  $("body").toggleClass("contact__expanded");
+  $("contact").toggleClass("contact__expanded");
+})
